1a4ade668d8752d767d12d3ff3bd0a29c8a925f6
huscker <12ye21geyy21gg@mail.ru>
2020-07-14 00:33:59 +0300

    переделал парсер аргументов используя библиотеку argparse, подготовил код к релизу

--- mainDecoder.py
+++ mainDecoder.py
@@ -1,7 +1,6 @@
 import decoder, sys
 
 if __name__ == '__main__':
-    args = sys.argv[1:]
-    main_decoder = decoder.Decoder(args)
-    main_decoder.parse_args()
+    main_decoder = decoder.Decoder()
+    main_decoder.parse_args(sys.argv)
     main_decoder.decode()
